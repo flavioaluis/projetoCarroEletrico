d49ada895b972faa7bf8449d3616006d4b0d92bf
Flávio <flavio.aluis@gmail.com>
2021-02-06 14:57:26 -0300

    trabalho-1take

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+$("#button").click(function(){
+    const rodados = $("#descricao").val();
+    if(!rodados)
+    return; //clausula guarda
+  if( rodados = 10)
+  gasolina = 1;
+  
+    
+})
+
+
+})

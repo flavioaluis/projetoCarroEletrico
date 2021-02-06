952c68e1601058ddd72def8ac54af8f5f16da6f1
Flávio <flavio.aluis@gmail.com>
2021-02-06 15:42:48 -0300

    trabalho-2take

--- scripts.js
+++ scripts.js
@@ -3,9 +3,18 @@ $("#button").click(function(){
     const rodados = $("#descricao").val();
     if(!rodados)
     return; //clausula guarda
-  if( rodados = 10)
-  gasolina = 1;
-  
+    var consumo1;
+    var reais1;
+    var consumo2;
+    var reais2;
+  consumo1 = rodados / 10 * 2 ;
+  reais1 =  consumo1 * 0,50;
+  consumo2 = rodados / 10;
+  reais2= consumo2 * 5;
+  $("#consumoenergia").prepend(`<li>${consumo1}<li>`);
+  $("#consumoenergia").prepend(`<li>${reais1}<li>`);
+  $("#gasolina").prepend(`<li>${consumo2}<li>`);
+  $("#gasreais").prepend(`<li>${reais2}<li>`);
     
 })
 

4d848fabdefd0c7a742c6ea0d8b262ff8f2cb596
Flávio <flavio.aluis@gmail.com>
2021-02-06 16:01:37 -0300

    trabalho-take6

--- scripts.js
+++ scripts.js
@@ -1,33 +1,42 @@
 $(document).ready(function(){
-$("#button1").click(function(){
-    const rodados1 = $("#descricao1").val();
-    if(!rodados1)
-    return; //clausula guarda
-    var consumo1;
-    var reais1;
-    
-  consumo1 = rodados1 / 10 * 2 ;
-  reais1 =  consumo1 * 0.50;
+   
+    $("#button1").click(function(){
+       const rodados1 = $("#descricao1").val();
+       var consumo1;
+       var reais1;
+   
+        consumo1 = rodados1 / 10 * 2 ;
+        reais1 =  consumo1 * 0.50;
   
-  $("#consumoenergia").prepend(`<li>${consumo1}<li>`);
-  $(consumo1).val();
-  $("#consumoenergia").prepend(`<li>${reais1}<li>`);
-  $(reais1).val();
+        if(!rodados1)
+        return; //clausula guarda
+  
+        $("#consumoenergia").prepend(`<li>${consumo1}<li>`);
+        $(consumo1).val();
+        $("#consumoenergia").prepend(`<li>${reais1}<li>`);
+        $(reais1).val();
 
-})
+});
 
-$("#button2").click(function(){
-    const rodados2 = $("#descricao2").val();
-    if(!rodados2)
-    return; //clausula guarda
-    var consumo2;
-    var reais2;
-  consumo2 = rodados2 / 10;
-  reais2 = consumo2 * 5;
-    $("#gasolina").prepend(`<li>${consumo2}<li>`);
-    $(consumo2).val();
-    $("#gasreais").prepend(`<li>${reais2}<li>`);
-    $(reais2).val();
+
+    $("#button2").click(function(){
+       const rodados2 = $("#descricao2").val();
+       var consumo2;
+       var reais2;
+
+        consumo2 = rodados2 / 10;
+        reais2 = consumo2 * 5;
+
+        if(!rodados2)
+        return; //clausula guarda
+
+        $("#gasolina").prepend(`<li>${consumo2}<li>`);
+        $(consumo2).val();
+        $("#gasreais").prepend(`<li>${reais2}<li>`);
+        $(reais2).val();
+
+
+});      
 
 
 })
